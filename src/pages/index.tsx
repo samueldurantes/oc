866e0659c36b5cad98a5f1b710c3c395890a930e
Samuel Durante <samueldurante.h@gmail.com>
2021-12-29 17:57:56 +0000

    feat: add preview your octopups

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useState, useRef, useEffect } from 'react'
 import * as ethers from 'ethers'
 
-import contract from '../../artifacts/contracts/FantomOctopups.sol/FantomOctopups.json'
+import Contract from '../../artifacts/contracts/FantomOctopups.sol/FantomOctopups.json'
 
 const Home: NextPage = () => {
   const [provider, setProvider] = useState<ethers.ethers.providers.Web3Provider | undefined>(undefined)
   const [value, setValue] = useState(1)
+  const [userTokens, setUserTokens] = useState<string[]>([])
+  const contract = useRef<ethers.ethers.Contract>()
 
   const handleConnectWallet = async () => {
     if (!window.ethereum) {
@@ -14,9 +16,40 @@ const Home: NextPage = () => {
       return
     }
 
+    if (window.ethereum.networkVersion != 4002) {
+      window.alert('Please connect to Fantom Opera Network')
+      return
+    }
+
     await window.ethereum.request({ method: 'eth_requestAccounts' })
     setProvider(new ethers.providers.Web3Provider(window.ethereum))
   }
+
+  const getUserTokens = async () => {
+    const tokens = []
+    let index = 0
+    const owner = window.ethereum.selectedAddress
+    const balance = await contract.current?.balanceOf(owner)
+    for (let i = 0; i < balance; i++) {
+      const token = await contract.current?.tokenOfOwnerByIndex(owner, index)
+      tokens.push(token.toString())
+      index++
+    }
+
+    setUserTokens(tokens)
+  }
+
+  useEffect(() => {
+    if (provider) {
+      contract.current = new ethers.Contract(
+        process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string,
+        Contract.abi, 
+        provider?.getSigner()
+      )
+
+      getUserTokens()
+    }
+  }, [provider])
 
   const changeValue = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(parseInt(event.target.value))
@@ -24,15 +57,8 @@ const Home: NextPage = () => {
 
   const handleMint = async () => {
     try {
-      const signer = provider?.getSigner()
-      const Contract = new ethers.Contract(
-        process.env.CONTRACT_ADDRESS as string,
-        contract.abi, 
-        signer
-      )
-
       console.log('Initialize payment')
-      const txn = await Contract.claim(value, {
+      const txn = await contract.current?.claim(value, {
         value: ethers.utils.parseEther((1.5 * value).toString())
       })
 
@@ -40,6 +66,7 @@ const Home: NextPage = () => {
       await txn.wait()
 
       console.log(txn.hash)
+      getUserTokens() // reload preview images
     } catch (error) {
       console.log(error)
     }
@@ -69,6 +96,11 @@ const Home: NextPage = () => {
           <div className='mt-12'>
             <h1 className='mb-4 text-4xl text-center'>Your Octupups:</h1>
             <div className='grid grid-cols-1 md:grid-cols-3 mt-6'>
+              {userTokens.map((id) => (
+                <div key={id}>
+                  <LoadImage src={id} />
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -85,6 +117,25 @@ const Home: NextPage = () => {
       )}
     </div>
   )
+}
+
+function LoadImage ({ src }: { src: string }) {
+  const [image, setImage] = useState(null)
+
+  useEffect(() => {
+    fetch(`/api/octopup/${src}`)
+      .then((response) => response.json())
+      .then((metadata) => {
+        const image = metadata.image?.replace('ipfs://', 'https://ipfs.io/ipfs/')
+
+        setImage(image)
+      })
+  })
+
+  if (!image) return <></>
+  
+  // eslint-disable-next-line @next/next/no-img-element
+  return <img className='h-64 w-64 pb-4 md:pr-4' key={image} src={image} alt={image} />
 }
 
 export default Home
